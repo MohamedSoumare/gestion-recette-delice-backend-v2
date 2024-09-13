eb550f301a32c617b58e8a5563a24d033b19f696
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-13 15:17:17 +0200

    update config and add file env

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,30 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
+
+// const connection = mysql.createConnection({
+//   host: localhost,
+//   user: root,
+//   password: root,
+//   database: gestion_recettes
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Database connected!');
+// });
+
+// module.exports = connection;
+
+
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: localhost,
-  user: root,
-  password: root,
-  database: gestion_recettes
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
@@ -14,3 +33,4 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+
