87b62367b622789a01732257d051b7b4163506da
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-25 00:34:35 +0200

    config of helpers

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -1,9 +1,8 @@
-
 import {
   DisplayProcessor,
   SpecReporter,
   StacktraceOption,
-} from "jasmine-spec-reporter";
+} from 'jasmine-spec-reporter';
 
 class CustomProcessor extends DisplayProcessor {
   displayJasmineStarted(info, log) {
@@ -20,4 +19,3 @@ jasmine.getEnv().addReporter(
     customProcessors: [CustomProcessor],
   })
 );
-
