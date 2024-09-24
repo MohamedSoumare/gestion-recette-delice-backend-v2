38a47fdc8363705790283530c0313aff6a9a2bf8
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-25 00:29:11 +0200

    modify doubl quote

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -2,16 +2,24 @@ import db from '../config/db.js';
 
 const Recipe = {
   create: async (title, type, description, ingredient) => {
-    const query = 'INSERT INTO recipes (title, type, description, ingredient) VALUES (?, ?, ?, ?)';
-    const [result] = await db.query(query, [title, type, description, ingredient]);
+    const query =
+      'INSERT INTO recipes (title, type, description, ingredient) VALUES (?, ?, ?, ?)';
+    const [result] = await db.query(query, [
+      title,
+      type,
+      description,
+      ingredient,
+    ]);
     return result;
   },
 
   checkRecipe: async (title) => {
-    const [rows] = await db.query('SELECT COUNT(*) as count FROM recipes WHERE title = ?', [title]);
+    const [rows] = await db.query(
+      'SELECT COUNT(*) as count FROM recipes WHERE title = ?',
+      [title]
+    );
     return rows[0].count; // Returns the number of recipes with this title
   },
-
 
   getById: async (id) => {
     const query = 'SELECT * FROM recipes WHERE id = ?';
@@ -19,25 +27,25 @@ const Recipe = {
     return rows.length > 0 ? rows[0] : null;
   },
 
+  update: async (id, updatedData) => {
+    const query =
+      'UPDATE recipes SET title = ?, type = ?, description = ?, ingredient = ? WHERE id = ?';
+    const [result] = await db.query(query, [
+      updatedData.title,
+      updatedData.type,
+      updatedData.description,
+      updatedData.ingredient,
+      id,
+    ]);
+    return result;
+  },
 
-update: async (id, updatedData) => {
-  const query = 'UPDATE recipes SET title = ?, type = ?, description = ?, ingredient = ? WHERE id = ?';
-  const [result] = await db.query(query, [
-    updatedData.title,
-    updatedData.type,
-    updatedData.description,
-    updatedData.ingredient,
-    id,
-  ]);
-  return result;
-},
-
-// Méthode de suppression
-delete: async (id) => {
-  const query = 'DELETE FROM recipes WHERE id = ?';
-  const [result] = await db.query(query, [id]);
-  return result; 
-},
+  // Méthode de suppression
+  delete: async (id) => {
+    const query = 'DELETE FROM recipes WHERE id = ?';
+    const [result] = await db.query(query, [id]);
+    return result;
+  },
 
   getAll: async () => {
     const query = 'SELECT * FROM recipes';
@@ -46,6 +54,4 @@ delete: async (id) => {
   },
 };
 
-
 export default Recipe;
-
