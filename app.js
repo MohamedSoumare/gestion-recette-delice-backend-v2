12b3ea62046b80e4a18f8584d538d1d059db7a6c
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-24 23:32:59 +0200

    modify doubl quote

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-
 import express from 'express';
-import bodyParser from 'body-parser'; 
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import db from './src/config/db.js'; 
-import recipeRoutes from './src/routes/recipeRoutes.js';
-
-
+import db from './src/config/db.js';
+import recipeRoutes from './src/routes/RecipeRoutes.js ';
 
 dotenv.config(); // Charge les variables d'environnement depuis un fichier .env
 
 const app = express();
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.use('/api', recipeRoutes); 
+app.use('/api', recipeRoutes);
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
