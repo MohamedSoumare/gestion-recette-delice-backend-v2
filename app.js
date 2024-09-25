5d24953f31efed88f808ea31559a2d058d360285
Mohamed Soumare <mohamedsoumare17763@gmail.com>
2024-09-25 16:30:49 +0200

    updated now all file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import RecipeRoutes from './src/routes/RecipeRoutes.js';
+import RecipeRoutes from './src/routes/RecipeRoutes.js ';
 
 dotenv.config();
 const app = express();
